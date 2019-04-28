e9414a64765469399cfd4e7716515b10693b1f3a
毛涛 <@DESKTOP-1CK2BIH>
2019-04-29 07:54:41 +0800

    111

---
+++ Esubao/Controllers/MT/HelpeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Esubao.Controllers.MT
+{
+    public class HelpeController : Controller
+    {
+        // GET: Helpe
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HelpeIndex()
+        {
+            return View();
+        }
+    }
+}
