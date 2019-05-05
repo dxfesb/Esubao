74b84942ac6e11942f8e8f29f5010cc41e8838db
罗文杰！ <@LAPTOP-D5RCMFTS>
2019-05-05 19:45:05 +0800

    1

---
+++ Esubao/Controllers/LWJ/BootController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Esubao.Controllers.LWJ
+{
+    public class BootController : Controller
+    {
+        // GET: Boot
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Wode()
+        {
+            return View();
+        }
+    }
+}
