ddb8904845cf388d9e13eecd6b48ff2267417e67
罗文杰！ <@LAPTOP-D5RCMFTS>
2019-05-05 19:26:39 +0800

    1

--- Esubao/App_Start/RouteConfig.cs
+++ Esubao/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Esubao
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Huayu", id = UrlParameter.Optional }
             );
         }
     }
