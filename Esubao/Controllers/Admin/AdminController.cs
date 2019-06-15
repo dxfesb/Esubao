f9e171871b0f44eefaf60963f0e680bcb7ec209b
lenom <lenom@DESKTOP-UA9HH2B>
2019-06-15 16:48:06 +0800

    31212

---
+++ Esubao/Controllers/Admin/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Esubao.Controllers.Admin
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+        /// <summary>
+        /// 后台登录页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login() {
+
+            return View();
+        }
+        /// <summary>
+        /// 后台首页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AdminIndex()
+        {
+
+            return View();
+        }
+       
+    }
+}
