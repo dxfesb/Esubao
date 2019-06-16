ba267dd141faa9bfb0e2d48fbbb214ff8c5ad0c0
罗文杰！ <@LAPTOP-D5RCMFTS>
2019-06-16 19:10:59 +0800

    1

---
+++ Esubao/Controllers/LWJ/MyforController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Esubao.Controllers.LWJ
+{
+    public class MyforController : Controller
+    {
+        // GET: Myfor
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MyInfor()
+        {
+            return View();
+        }
+    }
+}
